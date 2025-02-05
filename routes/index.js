ada423d8276d2243432e67d5ffd8d22798f32426
autumnchris <autumncbullard@gmail.com>
2025-02-05 18:15:14 -0500

    Add /weather and /cities routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const weatherController = require('../controllers/weather-controller');
+const citiesController = require('../controllers/cities-controller');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   res.send('Route to "/weather" to get weather data. Route to "/cities" to get city data.')
 });
+
+router.get('/weather', weatherController.fetchWeather);
+
+router.get('/cities', citiesController.fetchCities);
 
 module.exports = router;
