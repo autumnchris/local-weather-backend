1e74e086dea4e01e1a000633a82b34ef4e6908e0
autumnchris <autumncbullard@gmail.com>
2025-02-05 18:06:12 -0500

    Build cities controller

---
+++ controllers/cities-controller.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+
+exports.fetchCities = (req, res, next) => {
+  const { searchInput } = req.query;
+  const citiesData = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=20000&namePrefix=${searchInput}`;
+  const options = {
+    method: 'GET',
+    url: citiesData,
+    headers: {
+      'X-RapidAPI-Key': process.env.GEO_DB_API_KEY,
+      'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+  };
+
+  if (!searchInput) {
+    return res.status(400).send('Error: A search input of a city name is required');
+  }
+
+  axios.request(options)
+    .then(citiesResponse => {
+
+      if (citiesResponse.data.data.length === 0) {
+        return res.status(404).send('No cities were found matching the search input');
+      }
+
+      res.json({
+        cities: citiesResponse.data
+      });
+    }).catch(error => {
+      console.error('Error fetching city data:', error);
+      res.status(500).send('Error fetching city data from GeoDB Cities API');
+    });
+}
