50140e75cbbe4c9e81643e713fafc14544fd0f42
autumnchris <autumncbullard@gmail.com>
2025-02-05 19:25:49 -0500

    Update homepage instructions

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const citiesController = require('../controllers/cities-controller');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('Route to "/weather" to get weather data. Route to "/cities" to get city data.')
+  res.send(`
+    <h1>Instructions</h1>
+    <p>To get weather data, route to <code>https://autumnchris-local-weather-backend.onrender.com/weather?lat=&lt;your-latitude&gt;&lon=&lt;your-longitude&gt;</code>.</p>
+    <p>To get cities data, route to <code>https://autumnchris-local-weather-backend.onrender.com/cities?searchInput=&lt;your-city-search-input&gt;</code>.</p>
+  `);
 });
 
 router.get('/weather', weatherController.fetchWeather);
